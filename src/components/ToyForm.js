be2b99308558f3ba7189abc94c061520ef0c0c91
Riko-04 <echoge11@gmail.com>
2024-05-02 05:44:01 +0300

    Completes the GET and POST requests

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-function ToyForm() {
+function ToyForm({addaToy}) {
+  const[formdata, setFormData]=useState({
+    name:"",
+    image:"",
+  });
+
+  function handleChange(event) {
+    setFormData({
+      ...formdata,
+      [event.target.name] : event.target.value,
+    });
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const newToy ={
+      ...formdata,
+      likes:0,
+    }
+    axios
+    .post('http://localhost:3001/toys', newToy)
+    .then((res) => addaToy(res.data))
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
+          onChange={handleChange}
+          value={formdata.name}
           placeholder="Enter a toy's name..."
           className="input-text"
         />
@@ -15,6 +41,8 @@ function ToyForm() {
         <input
           type="text"
           name="image"
+          onChange={handleChange}
+          value={formdata.image}
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
